caa23943a83643a528aba3b28022063355327692
Niels Robin-Aubertin <git@niels.fr>
2023-04-04 11:44:00 +0200

    Address PR comments

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -16,6 +16,8 @@ import yaml
 from ops.model import ActiveStatus, Application
 from pytest import fixture
 from pytest_operator.plugin import OpsTest
+from boto3 import client
+from botocore.config import Config
 
 logger = logging.getLogger(__name__)
 
@@ -145,3 +147,31 @@ def localstack_s3_config(localstack_url: str) -> dict:
         "endpoint_without_scheme": f"{parsed_s3_url.hostname}:{parsed_s3_url.port}",
     }
     return s3_config
+
+
+@fixture(scope="module")
+def localstack_s3_client(localstack_s3_config: dict) -> client:
+    """Generate a s3_client.
+
+    Return a s3_client based on the localstack s3 config
+    """
+    # Configuration for boto client
+    s3_client_config = Config(
+        region_name=localstack_s3_config["region"],
+        s3={
+            "addressing_style": "virtual",
+        },
+    )
+
+    # Configure the boto client
+    s3_client = client(
+        "s3",
+        localstack_s3_config["region"],
+        aws_access_key_id=localstack_s3_config["credentials"]["access-key"],
+        aws_secret_access_key=localstack_s3_config["credentials"]["secret-key"],
+        endpoint_url=localstack_s3_config["endpoint"],
+        use_ssl=False,
+        config=s3_client_config,
+    )
+
+    return s3_client
