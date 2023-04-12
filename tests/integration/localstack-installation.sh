585a314ea56705cc2b4bd75965acc546ae56f575
Niels Robin-Aubertin <git@niels.fr>
2023-04-12 18:49:45 +0200

    Try fix localstack version to 1.4.0

--- tests/integration/localstack-installation.sh
+++ tests/integration/localstack-installation.sh
@@ -2,19 +2,14 @@
 
 pip install pip --upgrade
 pip install pyopenssl --upgrade
-pip install --upgrade localstack # install LocalStack cli
-docker pull localstack/localstack # Make sure to pull the latest version of the image
-id
-mkdir -p /var/lib/localstack
-chmod 777 /var/lib/localstack
+pip install localstack==1.4.0
+docker pull localstack/localstack:1.4.0
 ACTIVATE_PRO=0 \
   EDGE_BIND_HOST=0.0.0.0 \
   EDGE_PORT=4566 \
-  IMAGE_NAME=localstack/localstack:2.0.1 \
+  IMAGE_NAME=localstack/localstack:1.4.0 \
   DEBUG=1 \
-  LOCALSTACK_VOLUME_DIR=~/.cache/localstack/volume \
-  SERVICES=s3 \
-  localstack start -d # Start LocalStack in the background (binding to all host ip)
+  localstack start --docker -d # Start LocalStack in the background (binding to all host ip)
 echo "Waiting for LocalStack startup..." # Wait 30 seconds for the LocalStack container
 localstack wait -t 30 # to become ready before timing out 
 echo "Startup complete"
