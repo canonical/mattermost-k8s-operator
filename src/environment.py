f7714430700bacc30e5458dd1aa052c37621370b
arturo-seijas <102022572+arturo-seijas@users.noreply.github.com>
2024-07-11 10:36:26 +0200

    Fix license headers (#28)

--- src/environment.py
+++ src/environment.py
@@ -1,5 +1,5 @@
 """Generate container app environment based on charm configuration."""
-# Copyright 2023 Canonical Ltd.
+# Copyright 2024 Canonical Ltd.
 # See LICENSE file for licensing details.
 
 import json
