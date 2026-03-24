ae46764819d0888a4c49276afb2d199a4d24c71d
DeeKay3 <dkamar490@gmail.com>
2026-03-24 16:52:09 +0000

    feat/ISD-715: Changed rock so that it doesn't patch webapp with custom themes anymore and uses script for startup command. Added release artifact.

---
+++ mattermost_rock/start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+export MM_SQLSETTINGS_DRIVERNAME=postgres
+export MM_SQLSETTINGS_DATASOURCE="postgres://${POSTGRESQL_DB_CONNECT_STRING#postgresql://}"
+export MM_CONFIG="$MM_SQLSETTINGS_DATASOURCE"
+export MM_SERVICESETTINGS_LISTENADDRESS=:8080
+
+exec /app/bin/mattermost
