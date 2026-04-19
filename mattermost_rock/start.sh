eda8d168b82a989628a163392d2a80fcfaa2ab03
DeeKay3 <dkamar490@gmail.com>
2026-04-19 17:23:31 +0100

    Export ingress URL to MM siteurl env var

--- mattermost_rock/start.sh
+++ mattermost_rock/start.sh
@@ -9,6 +9,7 @@ export MM_SQLSETTINGS_DRIVERNAME=postgres
 export MM_SQLSETTINGS_DATASOURCE="postgres://${POSTGRESQL_DB_CONNECT_STRING#postgresql://}"
 export MM_CONFIG="$MM_SQLSETTINGS_DATASOURCE"
 export MM_SERVICESETTINGS_LISTENADDRESS=:8080
+export MM_SERVICESETTINGS_SITEURL="${APP_BASE_URL:-http://localhost:8080}"
 
 # S3 file storage configuration
 if [ -n "$S3_BUCKET" ]; then
